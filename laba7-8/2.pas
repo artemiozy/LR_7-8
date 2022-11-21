a5425c821602d4e2b297a62f6fa4031718929868
Artiick <117628635+artemiozy@users.noreply.github.com>
2022-11-21 09:53:08 +0300

    Add files via upload

---
+++ laba7-8/2.pas
@@ -0,0 +1,11 @@
+﻿var
+s,s_old,s_new: string;
+i,l_old: byte;
+begin
+write('Исходная строка: '); 
+readln(s);
+print(s, ' ,');
+print(s, ' ,');
+println(s);
+println('Кол-во символов = ',length(s)*3+2)
+end.
---
+++ laba7-8/5.pas
@@ -0,0 +1,15 @@
+﻿var
+  f, w: string; 
+  i, k, t: integer;
+
+begin
+  println('введите строку: ');
+  readln(f);
+  i := length(f);
+  w := f[i];
+  for k := 1 to i - 1 do
+  begin
+    t := pos(w, f); 
+    if f[k] = w then  println('позиция повтор: ', t);
+  end;
+end.
